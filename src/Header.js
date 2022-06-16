0dfe985b3cf5f852ec1d82cac5ee4f683fb2af75
Vishal Mishra <mishravishal2810@gmail.com>
2022-06-17 00:26:39 +0530

    bssss

--- src/Header.js
+++ src/Header.js
@@ -5,16 +5,13 @@ import LanguageIcon from "@material-ui/icons/Language";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Avatar } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import logo from "./images/logo.png";
 
 function Header() {
   return (
     <div className="header">
       <Link to="/">
-        <img
-          className="header__icon"
-          src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-          alt=""
-        />
+        <img className="header__icon" src={logo} alt="" />
       </Link>
 
       <div className="header__center">
